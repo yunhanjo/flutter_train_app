3d48d84f0ce3700e801af06c261ce53fd136d4dc
yunhanjo <gkswh0728@gmail.com>
2025-07-11 22:52:49 +0900

    stationlist design

--- lib/stationlistpage.dart
+++ lib/stationlistpage.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-
-//역이름리스트 컴포넌트화하기
 //기본 뒤로가기 버튼 사용 (자동)
 //Navigator.push로 화면을 이동한 후, 이전 화면으로 돌아가면 자동으로 뒤로가기 버튼이 나타납니다.
 
@@ -16,35 +14,34 @@ class StationlistPage extends StatelessWidget {
 
       body: Column(
         children: [
-            Container(
-                width: double.infinity,
-                height: 50,
-                decoration: BoxDecoration(
-                  border: Border(bottom: BorderSide(color: Colors.grey[300]!)),
-                ),
-                child: Text(
-                  '     수서',
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                ),
-              ),          
+            station('     수서'),
+            station('     동탄'),
+            station('     평택지제'),
+            station('     천안아산'),
+            station('     오송'),
+            station('     대전'),
+            station('     김천구미'),
+            station('     동대구'),
+            station('     경주'),
+            station('     울산'),
+            station('     부산'),
         ],
       ),
     );
   }
-}
 
-class StationList {
-  List<String> stationName = [
-    '     수서',
-    '     동탄',
-    '     평택지제',
-    '     천안아산',
-    '     오송',
-    '     대전',
-    '     김천구미',
-    '     동대구',
-    '     경주',
-    '     울산',
-    '     부산',
-  ];
+  Container station(String name) {
+    return Container(
+              width: double.infinity,
+              height: 50,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                border: Border(bottom: BorderSide(color: Colors.grey[300]!)),
+              ),
+              child: Text(
+                '$name',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+            );
+  }
 }
